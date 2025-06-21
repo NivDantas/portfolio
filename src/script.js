9c0aace20e0adfb47b540a842b70f7b461d70c26
NivDantas <nivdantas@icloud.com>
2025-06-21 15:14:45 -0400

    Version 1.0

--- src/script.js
+++ src/script.js
@@ -1,17 +1,72 @@
-const html = document.querySelector("#html");
-
-const darkButton = document.querySelector("#dark-mode");
-const lightButton = document.querySelector("#light-mode");
-darkButton.addEventListener("click", () => {
-  html.classList.remove("dark");
-  darkButton.classList.add("hidden");
-  lightButton.classList.remove("hidden");
+const toggleButton = document.querySelector("#theme-toggle");
+const darkButton = document.querySelector("#theme-toggle-dark-icon");
+const lightButton = document.querySelector("#theme-toggle-light-icon");
+const userLanguage = navigator.language || navigator.userLanguage;
+const groupTab = document.querySelector("#group-tab");
+const groupTabTwo = document.querySelector("#group-tab-two");
+const svgIcon = groupTab.querySelector("#svg");
+const svgIconTwo = groupTabTwo.querySelector("#svg-two");
+const expandableDiv = document.querySelector("#expandable");
+const expandableDivTwo = document.querySelector("#expandable-two");
+groupTab.addEventListener("click", () => {
+  if (!window.matchMedia("(min-width: 1024px)").matches) return;
+  if (event.target === expandableDiv || exParagraphOne.contains(event.target))
+    return;
+  if (expandableDiv.classList.contains("hidden")) {
+    expandableDiv.classList.add("grid");
+    expandableDiv.classList.remove("hidden");
+    svgIcon.classList.remove("motion-safe:lg:animate-bounce");
+    expandableDiv.appendChild(svgIcon);
+  } else {
+    expandableDiv.classList.remove("grid");
+    expandableDiv.classList.add("hidden");
+    svgIcon.classList.add("motion-safe:lg:animate-bounce");
+    groupTab.appendChild(svgIcon);
+  }
 });
-
-lightButton.addEventListener("click", () => {
-  html.classList.add("dark");
-  lightButton.classList.add("hidden");
+groupTabTwo.addEventListener("click", (event) => {
+  if (!window.matchMedia("(min-width: 1024px)").matches) return;
+  if (
+    event.target === expandableDivTwo ||
+    exParagraphTwo.contains(event.target)
+  )
+    return;
+  if (expandableDivTwo.classList.contains("hidden")) {
+    expandableDivTwo.classList.add("grid");
+    expandableDivTwo.classList.remove("hidden");
+    svgIconTwo.classList.remove("motion-safe:lg:animate-bounce");
+    expandableDivTwo.appendChild(svgIconTwo);
+  } else {
+    expandableDivTwo.classList.remove("grid");
+    expandableDivTwo.classList.add("hidden");
+    svgIconTwo.classList.add("motion-safe:lg:animate-bounce");
+    groupTabTwo.appendChild(svgIconTwo);
+  }
+});
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (
+    !("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)")
+  ).matches
+) {
   darkButton.classList.remove("hidden");
+} else {
+  lightButton.classList.remove("hidden");
+}
+
+toggleButton.addEventListener("click", () => {
+  darkButton.classList.toggle("hidden");
+  lightButton.classList.toggle("hidden");
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+  }
 });
 let typed = new Typed("#element", {
   strings: ["Nivaldo Dantas"],
@@ -25,7 +80,7 @@ const translations = {
   pt: {
     devTitle: "Desenvolvedor Full-stack",
     paragraph:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro doloremque repudiandae libero exercitationem nemo, sint consequatur quaerat delectus illo ipsam blanditiis non, sapiente cum qui corporis incidunt illum voluptatibus ratione.",
+      "Olá, me chamo Nivaldo Dantas — desenvolvedor full-stack atualmente formado em engenhenharia de software — com sólida base acadêmica e experiência prática no desenvolvimento de aplicações mobile e web.",
     titleOne: "Educação",
     edSubTitleOne: "Bacharelado em Engenharia de Software",
     edSubParagraphOne: "Instituto Federal do Amazonas - IFAM",
@@ -39,11 +94,16 @@ const translations = {
     lanSubParagraphOne: "Nativo",
     lanSubTitleTwo: "Inglês",
     lanSubParagraphTwo: "Avançado (C1)",
+    exParagraphOne:
+      "Atividade extracurricular em parceria do IFAM com a Samsung com finalidade de capacitar, aperfeiçoar e atualizar o estudante, garantindo a constante modernização das competências e atualizar saberes com foco no desempenho, aprimoramento focado em cursos de React, React Native, Programação para Dispositivos Móveis, Banco de Dados, entre outros, com a implementação projetos reais.",
+    exParagraphTwo:
+      "<li>Desenvolvimento e manutenção de aplicações web para setores internos.</li><li>Participação na concepção de novos projetos colaborando com equipes multidisciplinares para levantamento de requisitos e definição de funcionalidades.</li><li>Implementação de automações para otimizar fluxos de trabalho e reduzir tarefas manuais, utilizando scripts e integrações entre sistemas internos.</li><li>Utilização de ferramentas de Kanban para organização, acompanhamento e priorização das tarefas, promovendo eficiência nos fluxos de trabalho.</li><li>Participação em reuniões de planejamento e retrospectiva, contribuindo para o alinhamento das demandas e melhoria contínua dos processos.</li>",
+    footerText: "© 2025 Nivaldo Dantas. Todos os direitos reservados.",
   },
   en: {
     devTitle: "Fullstack Developer",
     paragraph:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro doloremque repudiandae libero exercitationem nemo, sint consequatur quaerat delectus illo ipsam blanditiis non, sapiente cum qui corporis incidunt illum voluptatibus ratione.",
+      "Hi, my name is Nivaldo Dantas — a full-stack developer currently studying software engineering — with a solid academic background and practical experience in mobile and web application development.",
     titleOne: "Education",
     edSubTitleOne: "Bachelor of Software Engineering",
     edSubParagraphOne: "Federal Institute of Amazonas - IFAM",
@@ -57,6 +117,11 @@ const translations = {
     lanSubParagraphOne: "Native",
     lanSubTitleTwo: "English",
     lanSubParagraphTwo: "Advanced (C1)",
+    exParagraphOne:
+      "Extracurricular activity in partnership between IFAM and Samsung with the aim of training, improving and updating the student, ensuring the constant modernization of skills and updating knowledge with a focus on performance, improvement focused on courses in React, React Native, Programming for Mobile Devices, Database, among others, with the implementation of real projects",
+    exParagraphTwo:
+      "<li>Development and maintenance of web applications for internal sectors.</li><li>Participation in the conception of new projects, collaborating with multidisciplinary teams to gather requirements and define functionalities.</li> <li>Implementing automation to optimize workflows and reduce manual tasks, using scripts and integrations between internal systems.</li><li>Use of Kanban tools to organize, monitor and prioritize tasks, promoting efficiency in workflows.</li> <li>Participation in planning and retrospective meetings, helping to align demands and continuously improve processes.</li>",
+    footerText: "© 2025 Nivaldo Dantas. Todos os direitos reservados.",
   },
 };
 
@@ -75,12 +140,12 @@ const lanSubTitleOne = document.querySelector("#lan-subtitle-one");
 const lanSubParagraphOne = document.querySelector("#lan-subparagraph-one");
 const lanSubTitleTwo = document.querySelector("#lan-subtitle-two");
 const lanSubParagraphTwo = document.querySelector("#lan-subparagraph-two");
-
-enButton.addEventListener("click", () => {
-  enButton.classList.add("text-white");
-  enButton.classList.remove("text-neutral-500");
-  ptButton.classList.add("text-neutral-500");
-  ptButton.classList.remove("text-white");
+const exParagraphOne = document.querySelector("#ex-paragraph-one");
+const exParagraphTwo = document.querySelector("#ex-paragraph-two");
+const footerText = document.querySelector("#footer-text");
+function enSite() {
+  enButton.classList.remove("opacity-50");
+  ptButton.classList.add("opacity-50");
   devTitle.innerText = translations.en.devTitle;
   paragraph.innerText = translations.en.paragraph;
   titleOne.innerText = translations.en.titleOne;
@@ -96,13 +161,19 @@ enButton.addEventListener("click", () => {
   lanSubParagraphOne.innerText = translations.en.lanSubParagraphOne;
   lanSubTitleTwo.innerText = translations.en.lanSubTitleTwo;
   lanSubParagraphTwo.innerText = translations.en.lanSubParagraphTwo;
-});
+  exParagraphOne.innerText = translations.en.exParagraphOne;
+  exParagraphTwo.innerHTML = translations.en.exParagraphTwo;
+  footerText.innerText = translations.en.footerText;
+}
+
+enButton.addEventListener("click", enSite);
+if (userLanguage.startsWith("en")) {
+  enSite();
+}
 
 ptButton.addEventListener("click", () => {
-  enButton.classList.add("text-neutral-500");
-  enButton.classList.remove("text-white");
-  ptButton.classList.add("text-white");
-  ptButton.classList.remove("text-neutral-500");
+  enButton.classList.add("opacity-50");
+  ptButton.classList.remove("opacity-50");
   devTitle.innerText = translations.pt.devTitle;
   paragraph.innerText = translations.pt.paragraph;
   titleOne.innerText = translations.pt.titleOne;
@@ -118,4 +189,7 @@ ptButton.addEventListener("click", () => {
   lanSubParagraphOne.innerText = translations.pt.lanSubParagraphOne;
   lanSubTitleTwo.innerText = translations.pt.lanSubTitleTwo;
   lanSubParagraphTwo.innerText = translations.pt.lanSubParagraphTwo;
+  exParagraphOne.innerText = translations.pt.exParagraphOne;
+  exParagraphTwo.innerHTML = translations.pt.exParagraphTwo;
+  footerText.innerText = translations.pt.footerText;
 });
